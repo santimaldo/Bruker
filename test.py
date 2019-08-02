3762c8ac85ee9c59534a7270d697a1edb742bc1d
Santiago Agustin Maldonado Ochoa <smaldonado@famaf.unc.edu.ar>
2019-08-02 17:57:28 -0300

    creo la rama VersionPreliminar para empezar un nuevo diseno en la rama master.

--- test.py
+++ test.py
@@ -3,15 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Datos import *
 
-path  = "../2019-07-31_Esferas_CantidadDeHumedad/3/"
+# en $olvia
+#path = "../2019-07-31_Esferas_CantidadDeHumedad/3/"
+# en $lily
+path = "S:/Doctorado/Dendritas/116MHz/2019-06-13_Dendritas/1/"
+
 
 
 datos = DatosProcesados(path)
 
-
-
 print(datos.acqus.NS)
-
 
 re = datos.espectro.real
 im = datos.espectro.imag
@@ -19,7 +20,7 @@ mod = datos.espectro.abs()
 
 print('graficando...')
 plt.figure()
-plt.plot(re,'b')
-plt.plot(im,'r')
+plt.plot(re, 'b')
+plt.plot(im, 'r')
 plt.plot(mod, 'g')
 plt.show()
