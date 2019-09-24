fb40aa03579847a7cd22520fc96671234b9bb13d
Santiago Agustin Maldonado Ochoa <smaldonado@famaf.unc.edu.ar>
2019-09-24 15:29:24 -0300

    Agregue documentacion

--- VoigtFit.py
+++ VoigtFit.py
@@ -10,9 +10,61 @@ import lmfit as lm
 
 class VoigtFit:
     """
-    DOCUMENTACION
+    Class VoigtFit:
+        Esta clase sirva para hacer ajuste de picos de los espectros, usando
+        la funcion Voigt. Puedo dar la opcion de no ajustar, para solo generar
+        el modelo.
+    
+    Attributes
+    ----------
+    
+    x : Array_like
+        Eje x
+    
+    y : Array_like
+        Eje y
+    
+    Npicos : int
+        Numero de picos a ajustar
+        
+    modelo : objeto de clase lmfit.Model
+        Aqui se guarda el modelo que se ajusta
+    
+    params : objeto de clase lmfit.Parameters
+        Guarda los parametros del modelo
+    
+    ajuste : objeto de clase lmfit.ModelResult
+        Resultado del ajuste usando model.fit
+        
+    Methods
+    -------
+    generar_modelo():
+        Como su nombre indica, genera el objeto Model. Lo que hace es agregar la
+        cantidad de picos necesarios.
+        ===================COSAS A RESOLVER 24/09/2019==================
+        ==    Implementar:                                            ==
+        ==        Dar parametros iniciales                            ==
+        ==        Fijar parametros o rangos                           ==
+        ================================================================
+    
+    fit():
+        Realiza el ajuste utilizando Model.fit(). El resultado se guarda en el
+        atributo ajuste, y el atributo params se modifica con los parametros
+        correspondientes al mejor ajuste.
+
+    componentes(x)
+        Devuelve los datos ajustados. Hay que darle el eje x.
+        return : total, componentes
+            total : vector con la funcion de ajuste.
+            componentes : lista con cada componente del ajuste, es decir, cada
+                pico.
+    
+    plot():
+        Es basicamente el metodo plot de la clase ModelResult. 
+        Grafica el resultado y los residuos.
+    
     """
-    def __init__(self, x, y, Npicos=1,**parametros_iniciales):
+    def __init__(self, x, y, Npicos=1, ajustar=True, **parametros_iniciales):
         
         self.x = x
         self.y = y
@@ -20,14 +72,12 @@ class VoigtFit:
         self.parametros_iniciales = parametros_iniciales
         self.modelo = None
         self.params = None
+        self.ajuste = None
+        
+        # ajusto:
         self.generar_modelo()
-
-    def UnMetodo(self):
-        """
-        @return  :
-        @author
-        """
-        pass
+        if ajustar:        
+            self.fit()
     
     def generar_modelo(self):
         
@@ -44,7 +94,6 @@ class VoigtFit:
         for i in range(Npicos):
             prefix_i = f'm{i+1}_'
             model =  lm.models.VoigtModel(prefix=prefix_i)
-            # if basis_func['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel']: # for now VoigtModel has gamma constrained to sigma
             model.set_param_hint('sigma', min=1e-6, max=x_range)
             model.set_param_hint('gamma', min=1e-6, max=x_range)
             model.set_param_hint('center', min=x_min, max=x_max)
@@ -76,3 +125,21 @@ class VoigtFit:
         # return:                
         self.modelo = modelo_compuesto
         self.params = params
+    #--------------------------------------------------------------------------
+        
+    def fit(self):
+        model = self.modelo            
+        self.ajuste = model.fit(self.y, self.params, x=self.x)
+        self.params = self.ajuste.params
+    #--------------------------------------------------------------------------    
+    def componentes(self, x):
+        total = self.modelo.eval(x=x, params=self.params)
+        comps = self.ajuste.eval_components(x=x)
+        componentes = []
+        for i in range(len(comps)):
+            componentes.append(comps[f'm{i+1}_'])
+
+        return total, componentes
+    #--------------------------------------------------------------------------
+    def plot(self):
+        self.ajuste.plot()
