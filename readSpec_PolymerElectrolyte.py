413c8e9f338aba2dd597ce48dd5d753c8e19bef5
santimaldo <santimaldonadoochoa@gmail.com>
2026-07-07 09:36:51 +0100

    Remove obsolete .pyc files and add VSCode settings for Python environment management.

--- readSpec_PolymerElectrolyte.py
+++ readSpec_PolymerElectrolyte.py
@@ -218,30 +218,119 @@ path_local = r"C:\Users\Santi\Documents\NMRdata\500/"
 savepath_local = r"C:\Users\Santi\OneDrive - University of Cambridge\Projects\PolymerElectrolyte\Analysis\2026-05_500MHz_PEO-LiTFSI_VT-phase-trans/"
 
 path_bruker = "2026-05-17_PEO-solid-electrolyte/"
-savepath_especifico = "13C_CP/"
-# # info: muestra, expn, ppmRange
+# savepath_especifico = "13C_CP/"
+# ## info: muestra, expn, ppmRange
+# nucleo = "13C"
+# ppmRange = [150,0]
+# infos = [['CP_contactTime_100us', 150, ppmRange],
+#          ['CP_contactTime_1000us', 151, ppmRange],
+#          ['CP_contactTime_10us', 152, ppmRange],
+#          ['CP_contactTime_200us', 153, ppmRange],
+#          ['CP_contactTime_3000us', 154, ppmRange],
+#          ['CP_contactTime_300us', 155, ppmRange],
+#          ['CP_contactTime_500us', 156, ppmRange],
+#          ['CP_contactTime_50us', 157, ppmRange],
+#          ]
+
+
+
+savepath_especifico = "13C_CP_vt/"
+## info: muestra, expn, ppmRange
 nucleo = "13C"
 ppmRange = [150,0]
-infos = [['CP_contactTime_100us', 150, ppmRange],
-         ['CP_contactTime_1000us', 151, ppmRange],
-         ['CP_contactTime_10us', 152, ppmRange],
-         ['CP_contactTime_200us', 153, ppmRange],
-         ['CP_contactTime_3000us', 154, ppmRange],
-         ['CP_contactTime_300us', 155, ppmRange],
-         ['CP_contactTime_500us', 156, ppmRange],
-         ['CP_contactTime_50us', 157, ppmRange],
-         ['CP_contactTime_20us', 158, ppmRange],
-         ]
+infos = []
+for expn in range(32, 133, 10):
+    infos.append(["", expn, ppmRange])
+infos.append(["", 77, ppmRange])
+infos.append(["", 87, ppmRange])
+infos.append(["", 97, ppmRange])
+infos.append(["", 108, ppmRange])
+infos.append(["", 116, ppmRange])
 
-# savepath_especifico = "PEO-PTT/"
+# savepath_especifico = "1H/"
 # # info: muestra, expn, ppmRange
 # nucleo = "1H"
 # ppmRange = [100,-100]
-# infos = [['PEO-PTT_Hahnecho', 11 , ppmRange]]
+# infos = []
+# Temps = [20, 30, 40, 30, 20, 10, 0, -10, 0, 10, 20]
+# for expn, temp in zip(range(31, 132, 10), Temps):
+#     infos.append([f"Hahnecho_{temp+273}K", expn, ppmRange])
+# infos.append([f"Hanecho_{30+273}K_bis", 46, ppmRange])
+# infos.append([f"Hanecho_{30+273}K_bis", 66, ppmRange])
+# infos.append([f"Hanecho_{20+273}K_bis", 76, ppmRange])
+# infos.append([f"Hanecho_{10+273}K_bis", 86, ppmRange])
+# infos.append([f"Hanecho_{0+273}K_bis", 96, ppmRange])
+# infos.append([f"Hanecho_{-10+273}K_d1-20s", 106, ppmRange])
+# infos.append([f"Hanecho_{-10+273}K_d1-30s", 107, ppmRange])
+
+# savepath_especifico = "19F/"
 # # info: muestra, expn, ppmRange
 # nucleo = "19F"
-# ppmRange = [100,-300]
-# infos = [['PEO-PTT_zg', 1 , ppmRange]]
+# ppmRange = [100, -300]
+# infos = []
+# Temps = [20, 30, 40, 30, 20, 10, 0, -10, 0, 10, 20]
+# for expn, temp in zip(range(30, 131, 10), Temps):
+#     infos.append([f"Hahnecho_{temp+273}K", expn, ppmRange])
+
+# infos.append([f"Hanecho_{30+273}K_bis", 47, ppmRange])
+# infos.append([f"Hanecho_{10+273}K_bis", 88, ppmRange])
+# infos.append([f"Hanecho_{0+273}K_bis", 98, ppmRange])
+# infos.append([f"Hanecho_{-10+273}K_bis", 109, ppmRange])
+# infos.append([f"Hanecho_{0+273}K_bis", 117, ppmRange])
+# infos.append([f"Hanecho_{10+273}K_bis", 126, ppmRange])
+# infos.append([f"Hanecho_{20+273}K_bis", 136, ppmRange])
+
+
+# savepath_especifico = "7Li/"
+# # info: muestra, expn, ppmRange
+# nucleo = "7Li"
+# ppmRange = [250, -250]
+# infos = [
+#     ['zg_293K', 33, ppmRange], # 20 degC
+#     ['zgdec_293K', 34, ppmRange],
+#     ['zg_293K_longerAcq', 35, ppmRange],
+
+#     ['zg_303K', 43, ppmRange], # 30 degC
+#     ['zgdec_303K', 44, ppmRange],
+#     ['zg_303K_longerAcq', 45, ppmRange],
+
+#     ['zg_313K', 53, ppmRange], # 40 degC
+#     ['zgdec_313K', 54, ppmRange],
+#     ['zg_313K_longerAcq', 55, ppmRange],
+#     ['zg_313K_evenLongerAcq', 56, ppmRange],
+
+#     ['zg_303K', 63, ppmRange], # 30 degC
+#     ['zgdec_303K', 64, ppmRange],
+#     ['zg_303K_longerAcq', 65, ppmRange],
+
+#     ['zg_293K', 73, ppmRange], # 20 degC
+#     ['zgdec_293K', 74, ppmRange],
+#     ['zg_293K_longerAcq', 75, ppmRange],
+
+#     ['zg_283K', 83, ppmRange], # 10 degC
+#     ['zgdec_283K', 84, ppmRange],
+#     ['zg_283K', 85, ppmRange],
+
+#     ['zg_273K', 93, ppmRange], # 0 degC
+#     ['zgdec_273K', 94, ppmRange],
+#     ['zg_273K', 95, ppmRange],
+
+#     ['zg_263K', 103, ppmRange], # -10 degC
+#     ['zgdec_263K', 104, ppmRange],
+#     ['zg_263K', 105, ppmRange],
+
+#     ['zg_273K', 113, ppmRange], # 0 degC
+#     ['zgdec_273K', 114, ppmRange],
+#     ['zg_273K', 115, ppmRange],
+
+#     ['zg_283K', 123, ppmRange], # 10 degC
+#     ['zgdec_283K', 124, ppmRange],
+#     ['zg_283K', 125, ppmRange],
+
+#     ['zg_293K', 133, ppmRange], # 20 degC
+#     ['zgdec_293K', 134, ppmRange],
+#     ['zg_293K', 135, ppmRange],
+# ]
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -252,6 +341,8 @@ fig, ax = plt.subplots(num=1, nrows=1, ncols=1)  # create figure & 1 axis
 for info in infos:
     muestra, expn, ppmRange= info
     save = True
+    muestra = ""
+    print("WARNING!!!! Sobreescribiendo muestra a vacio para que no guarde el nombre de la muestra en el archivo de datos")
 
 
     # extraigo:
@@ -275,13 +366,14 @@ for info in infos:
     ax.set_title(muestra)
     ax.set_xlabel(f"{nucleo} NMR Shift [ppm]")
     # ax.set_xlim([np.max(ppmAxis), np.min(ppmAxis)])
-
+    if muestra != "":
+        muestra = f"_{muestra}"
     if save:
         savepath = f"{savepath_local}{savepath_especifico}"
         # guardo:
         header = "ppmAxis\t real \t imag"
         dataexport = np.array([ppmAxis, re, im]).T
-        filename = f'{savepath}/{nucleo}_Nexp{expn}_{muestra}.dat'
+        filename = f'{savepath}/{nucleo}_Nexp{expn}{muestra}.dat'
         np.savetxt(filename, dataexport, header=header)
 
         # filename = f'{savepath}/{nucleo}_{muestra}.png'
